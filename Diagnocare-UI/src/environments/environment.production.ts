8b2810a66ff659206110cdf1db3b93cf1c84c030
Vanchhit Kumar <kumarvanchhit47@gmail.com>
2026-09-19 23:03:17 +0530

    Updated prod basic password

--- Diagnocare-UI/src/environments/environment.production.ts
+++ Diagnocare-UI/src/environments/environment.production.ts
@@ -14,6 +14,6 @@ export const environment: AppEnvironment = {
 
   basicAuth: {
     username: 'Admin',
-    password: 'OwJ3dA38hJuNHsEBTxXju6JB5qAZNNiTvWDrnZOBSXY=',
+    password: 'ggDgc+q0Y4xNWOadnfALUOEEi/ijWn4I0fd06Keor5Y=',
   },
 };
